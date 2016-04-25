a642d96de990c1c7af36d924978cf3e74f0b22c4
Tatsiana Synkova <tvoyangel.by@mail.ru>
2016-04-25 10:59:06 +0300

    updated configurations

--- src/main/java/org/tatsiana/webapp/ApplicationInitializer.java
+++ src/main/java/org/tatsiana/webapp/ApplicationInitializer.java
@@ -3,11 +3,9 @@ package org.tatsiana.webapp;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.DispatcherServlet;
 import org.tatsiana.webapp.config.WebConfig;
 
-import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
@@ -16,8 +14,6 @@ public class ApplicationInitializer implements WebApplicationInitializer {
 
     private static final String DISPATCHER = "dispatcher";
 
-    private static final String CHARSET_FILTER = "charsetFilter";
-
 
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
@@ -25,14 +21,6 @@ public class ApplicationInitializer implements WebApplicationInitializer {
         context.register(WebConfig.class);
 
         servletContext.addListener(new ContextLoaderListener(context));
-
-
-        FilterRegistration.Dynamic characterEncodingFilter = servletContext.addFilter(CHARSET_FILTER,
-                new CharacterEncodingFilter());
-        characterEncodingFilter.setInitParameter("encoding", "UTF-8");
-        characterEncodingFilter.setInitParameter("forceEncoding", "true");
-        characterEncodingFilter.addMappingForUrlPatterns(null, true, "/*");
-
 
         ServletRegistration.Dynamic dispatcher = servletContext.addServlet(DISPATCHER, new DispatcherServlet(context));
         dispatcher.addMapping("/");
